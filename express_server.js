42bb24f5335b9a636f952d141bca15f842bae6b4
shivangipatodiya <shivu2610@gmail.com>
2021-09-28 02:38:03 +0000

    endpoints add

--- express_server.js
+++ express_server.js
@@ -2,18 +2,25 @@ const express = require('express');
 const app = express();
 const PORT = 8080;
 
-// const urlDatabase = {
-//   "b2xVn2": "http://www.lighthouselabs.ca",
-//   "9sm5xK": "http://www.google.com"
-// };
+const urlDatabase = {
+  "b2xVn2": "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com"
+};
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
-// app.get("/", (req, res) => {
-//   res.send("Hello!");
-// });
 
-// app.listen(PORT, () => {
-//   console.log(`Example app listening on port ${PORT}!`);
-// });
+app.get("/", (req, res) => {
+  res.send("Hello!");
+});
+
+app.get("/urls.json", (req, res) => {
+  res.send(urlDatabase)
+});
+
+app.get("/hello", (req, res) => {
+  res.send("<html><body>Hello <b>World</b></body></html>\n")
+})
+
+// 
