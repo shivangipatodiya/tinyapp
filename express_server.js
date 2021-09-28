427044d5759608cd43c69013aafe9924a7c86f81
shivangipatodiya <shivu2610@gmail.com>
2021-09-28 03:29:09 +0000

    add views with template

--- express_server.js
+++ express_server.js
@@ -25,4 +25,9 @@ app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n")
 })
 
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+})
+
 // 
